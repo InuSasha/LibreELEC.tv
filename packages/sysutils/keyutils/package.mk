e3ce79051859078d21b4173f9df1a4ff54f1b2ac
Sascha Kuehndel (InuSasha) <dev@inusasha.de>
2019-03-18 09:19:19 +0100

    buildsystem: activate hardening flags - based on the flags used in ubuntu: https://wiki.ubuntu.com/ToolChain/CompilerFlags - drop PIE, because it require PIC

--- packages/sysutils/keyutils/package.mk
+++ packages/sysutils/keyutils/package.mk
@@ -10,7 +10,7 @@ PKG_SITE="http://people.redhat.com/~dhowells/keyutils/"
 PKG_URL="http://people.redhat.com/~dhowells/keyutils/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Keyutils is a set of utilities for managing the key retention facility in the kernel."
-PKG_BUILD_FLAGS="+pic"
+PKG_BUILD_FLAGS="+pic -hardening"
 
 PKG_MAKE_OPTS_TARGET="NO_ARLIB=0 NO_SOLIB=1 BINDIR=/usr/bin SBINDIR=/usr/sbin LIBDIR=/usr/lib USRLIBDIR=/usr/lib"
 PKG_MAKEINSTALL_OPTS_TARGET="$PKG_MAKE_OPTS_TARGET"
